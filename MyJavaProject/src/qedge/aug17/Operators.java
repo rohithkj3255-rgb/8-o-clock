05ba5a78136e623c8909779c5820a1cd02a55218
rohit <rohit@Simon>
2025-10-25 20:21:52 +0530

    FIRST COMMIT

---
+++ MyJavaProject/src/qedge/aug17/Operators.java
@@ -0,0 +1,13 @@
+package qedge.aug17;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		System.out.println(a);
+		// TODO Auto-generated method stub
+
+	}
+
+}
